12c5f7e5407ad431471990fdd0f741e1ab0d731e
liangangel1 <angell213@nycstudents.net>
2024-05-09 13:58:10 -0400

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -5,4 +5,36 @@ class shrub:
          self. damage = damage
 
 class slime (shrub):
-     def __init__ (self, hp, level, damage, posion)
+     def __init__ (self, hp, level, damage, poison):
+          super().__init__(hp, level, damage)
+          self.poison=poison
+     def __str__(self):
+          return f"{self.hp}, {self.level}, {self.damage}, {self.poison}"
+     
+class goblin (slime):
+     def __init__ (self, hp, level, damage, poison, double):
+          super().__init__(hp, level, damage, poison)
+          self.double = double
+     def __str__(self):
+          return f"{self.hp}, {self.level}, {self.damage}, {self.poison}, {self.double}"
+     
+class orc (goblin):
+     def __init__ (self, hp, level, damage, poison, double, less):
+          super().__init__(hp, level, damage, poison, double)
+          self.less=less
+     def __str__(self):
+          return f"{self.hp}, {self.level}, {self.damage}, {self.poison}, {self.double}, {self.less}"
+
+class dungeon_lord (orc):
+     def __init__ (self, hp, level, damage, poison, double, less, heal):
+          super().__init__(hp, level, damage, poison, double, less)
+          self.heal=heal
+     def __str__(self):
+          return f"{self.hp}, {self.level}, {self.damage}, {self.poison}, {self.double}, {self.less}, {self.heal}"
+
+
+shrubs = shrub ("30HP", "Level 1", "10 damage")
+slimes =  slime ("50HP", "Level 2", "15 damage", "5 additional damage")
+goblins = goblin ("70HP", "Level 3", "25 damage", "5 additional damage", ) 
+orcs=  orc ("90HP", "Level 4", "35 damagae")
+dungeon_lords = dungeon_lord ("140HP", "Level 5", "50 damage")
