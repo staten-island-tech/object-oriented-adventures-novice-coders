d9cbe441de791dd9c09a907d2a5fcaa675843a63
liangangel1 <angell213@nycstudents.net>
2024-05-09 13:31:12 -0400

    Create enemy.py

---
+++ enemy.py
@@ -0,0 +1,8 @@
+class shrub:
+    def __init__ (self, hp, level, damage):
+         self.hp = hp
+         self.level = level
+         self. damage = damage
+
+class slime (shrub):
+     def __init__ (self, hp, level, damage, posion)
