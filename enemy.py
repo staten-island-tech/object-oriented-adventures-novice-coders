fbed3fcbbf7c1bee189a9b3668be2dc9b9e7c9f1
liangangel1 <angell213@nycstudents.net>
2024-05-10 13:57:39 -0400

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -7,7 +7,8 @@ class shrub:
 class slime (shrub):
      def __init__ (self, hp, level, damage, poison):
           super().__init__(hp, level, damage)
-          self.poison=poison
+          self.poison = poison
+          poison=5
      def __str__(self):
           return f"{self.hp}, {self.level}, {self.damage}, {self.poison}"
      
@@ -33,8 +34,14 @@ class dungeon_lord (orc):
           return f"{self.hp}, {self.level}, {self.damage}, {self.poison}, {self.double}, {self.less}, {self.heal}"
 
 
-shrubs = shrub ("30HP", "Level 1", "10 damage")
-slimes =  slime ("50HP", "Level 2", "15 damage", "5 additional damage")
-goblins = goblin ("70HP", "Level 3", "25 damage", "5 additional damage", ) 
-orcs=  orc ("90HP", "Level 4", "35 damagae")
-dungeon_lords = dungeon_lord ("140HP", "Level 5", "50 damage")
+"""shrubs = shrub ("30HP", "Level 1", "10 damage")
+slimes =  slime ("50HP", "Level 2", "15 damage", "5 damage" )
+goblins = goblin ("70HP", "Level 3", "25 damage", "5 damage", "30 damage") 
+orcs=  orc ("90HP", "Level 4", "35 damage", "5 damage", "40 damage", "_____")
+dungeon_lords = dungeon_lord ("140HP", "Level 5", "50 damage", "_____", "_____", "_____", "_____")"""
+
+
+#2.0
+
+shrubs = shrub (30, 1, 10)
+slimes = slime (50, 2, 15, 5)
