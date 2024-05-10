572025cbd2ed91c9d1a3fcd0453cd0b5710e2550
hyacinths147 <caterynac@nycstudents.net>
2024-05-10 13:56:44 -0400

    Update user.py

--- user.py
+++ user.py
@@ -1,11 +1,10 @@
 class user:
-    def __init__(self, name, health, lvl):
+    def __init__(self, name, health, lvl, xp):
         self.name = name
         self.health = health
         self.lvl = lvl
+        self.xp = xp
     def __str__(self):
-        return f"{self.name}, {self.health}, {self.lvl}"
-    
-def player():
+        return f"{self.name}, {self.health}, {self.lvl}, {self.xp}"
     name = input("Enter in a name please: ")
-    
+
