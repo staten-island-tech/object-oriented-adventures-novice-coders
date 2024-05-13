a77f142d71bb74a6ddf935ec8c8634cbd8cad46c
liangangel1 <angell213@nycstudents.net>
2024-05-13 13:41:55 -0400

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -1,3 +1,5 @@
+#Characteristics of each enemy (NOT INCLUDING UPDATING HP PER ATTACK)
+
 class shrub:
     def __init__ (self, hp, level, damage):
          self.hp = hp
@@ -33,15 +35,17 @@ class dungeon_lord (orc):
      def __str__(self):
           return f"{self.hp}, {self.level}, {self.damage}, {self.poison}, {self.double}, {self.less}, {self.heal}"
 
-
-"""shrubs = shrub ("30HP", "Level 1", "10 damage")
-slimes =  slime ("50HP", "Level 2", "15 damage", "5 damage" )
-goblins = goblin ("70HP", "Level 3", "25 damage", "5 damage", "30 damage") 
-orcs=  orc ("90HP", "Level 4", "35 damage", "5 damage", "40 damage", "_____")
-dungeon_lords = dungeon_lord ("140HP", "Level 5", "50 damage", "_____", "_____", "_____", "_____")"""
-
-
 #2.0
+#format: hp, lvl, dmg, poison, 2x, 20% less hit, heal
 
 shrubs = shrub (30, 1, 10)
 slimes = slime (50, 2, 15, 5)
+goblins = goblin (70, 3, 25, 5, 2)
+orcs= orc (90, 4, 35, 5, 2, 0.8)
+dungeon_lords = dungeon_lord (140, 5, 50, 5, 2, 0.8, 15)
+
+#print (dungeon_lords.heal) #print specific value
+"""totaldmgDL= (dungeon_lords.damage + dungeon_lords.poison)*2
+print (totaldmgDL)"""
+#find total of some factor
+
